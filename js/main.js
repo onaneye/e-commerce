82de85f8d196d3fd4960870660de9845a3a17168
onaneye <66856751+onaneye@users.noreply.github.com>
2023-04-04 18:42:30 +0100

    Delete js directory

--- js/main.js
+++
@@ -1,6 +0,0 @@
-const bars = document.querySelector(".fa-bars")
-var container = document.querySelector("div.nav-col-item")
-
-bars.addEventListener("click", function() {
-    container.classList.toggle("toogle")
-})
